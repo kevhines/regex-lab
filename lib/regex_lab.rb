d8c2ef18592363d0953a822002efd96a7c729dbb
Kevin <kevhines@gmail.com>
2021-01-22 16:18:10 -0500

    commit tree c0ba12b8902ee7c2152a59905240984f99d5acfa

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
+require "pry"
 def starts_with_a_vowel?(word)
-
+    word.match(/^[aeiouAEIOU]\w+/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/\bun\S+\ing\b/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.match(/\A[A-Z].+[.?!]\z/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  #  binding.pry
+    phone.scan(/[0-9]/).size == 10 ? true : false
+    
 end
